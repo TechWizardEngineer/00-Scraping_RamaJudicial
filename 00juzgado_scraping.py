ad981edb063666c466482495a1d4fddad104099b
Daniel MV <danmv@users.noreply.github.com>
2021-03-19 00:33:02 -0700

    Update 00juzgado_scraping.py

--- 00juzgado_scraping.py
+++ 00juzgado_scraping.py
@@ -91,6 +91,18 @@ data_dir = "project/data/"
 #!pip install unicode
 
 
+def explicit():
+    from google.cloud import storage
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        'service_account.json')
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
 """## Functions that would eventually go to classes (Monolithic Solution) to use in different sessions"""
 
 # Loading sheets from GoogleSheets in GoogleDrive
