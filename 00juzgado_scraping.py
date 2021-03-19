9bad7dfa5df6ac7d08afee0666f135cd9b726581
Daniel MV <danmv@users.noreply.github.com>
2021-03-19 00:35:42 -0700

    Update 00juzgado_scraping.py

--- 00juzgado_scraping.py
+++ 00juzgado_scraping.py
@@ -3,6 +3,7 @@ from pydrive.drive import GoogleDrive
 # from google.colab import auth
 # auth.authenticate_user()
  
+
 import gspread
 from oauth2client.client import GoogleCredentials
 #gc = gspread.authorize(GoogleCredentials.get_application_default())
