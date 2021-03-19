fb1811ed0fb874e64605522c2e9ba58f88b74879
Daniel MV <danmv@users.noreply.github.com>
2021-03-19 00:51:02 -0700

    Update 00juzgado_scraping.py

--- 00juzgado_scraping.py
+++ 00juzgado_scraping.py
@@ -98,7 +98,7 @@ def explicit():
     # Explicitly use service account credentials by specifying the private key
     # file.
     storage_client = storage.Client.from_service_account_json(
-        'service_account.json')
+        '/runner/.config/gspread/service_account.json')
 
     # Make an authenticated API request
     buckets = list(storage_client.list_buckets())
