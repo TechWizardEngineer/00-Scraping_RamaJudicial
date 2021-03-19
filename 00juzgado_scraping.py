cc5972f4532e688fe4523fc83bf55e93f09188b2
Daniel MV <danmv@users.noreply.github.com>
2021-03-18 22:35:36 -0700

    Update 00juzgado_scraping.py

--- 00juzgado_scraping.py
+++ 00juzgado_scraping.py
@@ -1,3 +1,35 @@
+gauth = GoogleAuth()
+
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+
+if gauth.credentials is None:
+    # Authenticate if they're not there
+
+    # This is what solved the issues:
+    gauth.GetFlow()
+    gauth.flow.params.update({'access_type': 'offline'})
+    gauth.flow.params.update({'approval_prompt': 'force'})
+
+    gauth.LocalWebserverAuth()
+
+elif gauth.access_token_expired:
+
+    # Refresh them if expired
+
+    gauth.Refresh()
+else:
+
+    # Initialize the saved creds
+
+    gauth.Authorize()
+
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")  
+
+drive = GoogleDrive(gauth)
+
+
 from google.colab import auth
 auth.authenticate_user()
  
